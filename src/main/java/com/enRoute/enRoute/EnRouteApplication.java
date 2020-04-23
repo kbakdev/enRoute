435e7fe06f6e4ecfe89e28dcb6ed0448a1d3dd4e
Sejki <bonczalke@gmail.com>
2020-04-23 18:08:30 +0200

    Test
    Test

---
+++ src/main/java/com/enRoute/enRoute/EnRouteApplication.java
@@ -0,0 +1,13 @@
+package com.enRoute.enRoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enRoute/enRoute/EnRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enRoute.enRoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
