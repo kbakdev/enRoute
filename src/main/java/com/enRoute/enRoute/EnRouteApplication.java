f92102f6e114ebf79fb49c5a027312ab380f51a4
Sejki <bonczalke@gmail.com>
2020-05-15 22:59:21 +0200

    Fix
    Little fix

--- src/main/java/com/enRoute/enRoute/EnRouteApplication.java
+++ src/main/java/com/enRoute/enRoute/EnRouteApplication.java
@@ -1,14 +1,12 @@
 package com.enRoute.enRoute;
 
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.sql.DataSource;
 
 
 @SpringBootApplication
